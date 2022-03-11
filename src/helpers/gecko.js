3062dddf73bc32e33f5a98ce8dddd9f3226da00b
asad <asadullahkust@gmail.com>
2022-03-11 23:06:38 +0500

    fixed linting in gecko api file

--- src/helpers/gecko.js
+++ src/helpers/gecko.js
@@ -3,11 +3,16 @@ const config = require('../../config/index');
 const errors = require('./errors');
 
 module.exports = {
+  extractedPrice: (data, currency = 'usd') => {
+    if (data.market_data && data.market_data.current_price) return data.market_data.current_price[currency];
+    return -1;
+  },
+
   getCoinByName:  async (coinName) => { 
     const apiEnd = config.URL.GECKO_API_URL + '/coins/' + coinName;
     try {
       const res = await request.get(apiEnd);
-      return res.body?.market_data?.current_price?.usd || -1;
+      return this.extractedPrice(res.body);
     } catch (e) {
       errors.assertExposable(null, 'unknown_coin_code');
     }
